fac43afdc5ced32936d1c2b93cbdf796d721356a
Buster422 <benjamin@gofranks.com>
2018-06-11 11:46:07 -0400

    Verb Stuff

---
+++ src/Game.java
@@ -0,0 +1,97 @@
+public class Game {
+
+    public void verbCheck(String verb){
+
+        switch(verb.toLowerCase()){
+
+            case "look":
+
+                break;
+
+            case "examine":
+            case "check":
+            case "inspect":
+            case "investigate":
+            case "study":
+
+                //to examine object
+
+                break;
+
+            case "take":
+            case "collect":
+            case "grab":
+            case "hold":
+            case "pick up":
+
+                //to object
+
+                break;
+
+            case "walk":
+            case "step":
+            case "move":
+            case "go":
+
+                //north, south, east, west
+
+                break;
+
+            case "put":
+            case "insert":
+            case "place":
+            case "set":
+            case "use":
+
+                //use object in another
+
+                break;
+
+            case "give":
+
+                break;
+
+            case "eat":
+
+                break;
+
+            case "sleep":
+
+                break;
+
+            case "sit":
+
+                break;
+
+            case "switch":
+
+                break;
+
+            case "smell":
+
+                break;
+
+            case "taste":
+
+                break;
+
+            case "listen":
+
+                break;
+
+            case "show":
+
+                break;
+
+            case "open":
+
+                break;
+
+            case "say":
+                //when we get chat function
+                break;
+        }
+
+    }
+
+}
---
+++ src/Item.java
@@ -0,0 +1,30 @@
+public class Item {
+
+    String desc; //examine
+
+    boolean grabable;
+
+    public void Item(String _desc, boolean _grabable){
+
+        desc = _desc;
+        grabable = _grabable;
+
+    }
+
+    public void look(){
+
+    }
+
+    public void examine(){
+
+    }
+
+    public void take(){
+
+    }
+
+    public void move(){
+
+    }
+
+}
