6c93cec5af655d598d66a5654faf1a57e9e7df88
Buster422 <benjamin@gofranks.com>
2018-06-12 10:13:15 -0400

    XD

--- src/Client.java
+++ src/Client.java
@@ -1,21 +1,21 @@
 import java.net.*;
+import java.util.Scanner;
 
 public class Client {
 
 
     public static void main(String args[]) throws Exception {
         DatagramSocket socket = new DatagramSocket();
-        int i = 0;
+        Scanner s = new Scanner(System.in);
         DatagramPacket request = new DatagramPacket(new byte[1], 1, new InetSocketAddress("10.0.0.134", 7777));
         while (true) {
+            request.setData(s.nextLine().getBytes());
             socket.send(request);
             byte[] buffer = new byte[512];
             DatagramPacket response = new DatagramPacket(buffer, buffer.length);
             socket.receive(response);
             String response_string = new String(buffer, 0, response.getLength());
-
-            System.out.println(response_string + " number " + i);
-            i++;
+            System.out.println(response_string);
             Thread.sleep(1000);
         }
 
